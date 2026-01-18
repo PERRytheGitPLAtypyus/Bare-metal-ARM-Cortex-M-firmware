c4d351d883ae485fb923610d4ebe6d8582a6f4d1
PERRytheGitPLAtypyus <154216592+PERRytheGitPLAtypyus@users.noreply.github.com>
2026-01-18 14:57:40 +0530

    Add files via upload

--- baremetal/bootflow/startup.c
+++ baremetal/bootflow/startup.c
@@ -1,70 +1,40 @@
+#include <stdint.h>
+
+/* Symbols from linker */
+extern uint32_t _estack;
 extern int main(void);
 
-/* Linker symbols */
-extern unsigned int _sidata;
-extern unsigned int _sdata;
-extern unsigned int _edata;
-extern unsigned int _sbss;
-extern unsigned int _ebss;
-
-/* Global SysTick counter */
-volatile unsigned int systick_count = 0;
-
-/* Default handler */
-void Default_Handler(void) {
-    while (1);
-}
-
-/* SysTick handler */
-void SysTick_Handler(void) {
-    systick_count++;
-}
-void PendSV_Handler(void) {
-    volatile int i;
-    for (i = 0; i < 100000; i++) {
-        /* intentional delay */
-    }
-}
-
-/* Reset handler */
-void Reset_Handler(void) {
-    unsigned int *src;
-    unsigned int *dst;
-
-    /* Copy .data from FLASH to RAM */
-    src = (unsigned int *)&_sidata;
-    dst = (unsigned int *)&_sdata;
-    while (dst < (unsigned int *)&_edata) {
-        *dst++ = *src++;
-    }
-
-    /* Zero .bss */
-    dst = (unsigned int *)&_sbss;
-    while (dst < (unsigned int *)&_ebss) {
-        *dst++ = 0;
-    }
-
-    /* Enter main */
-    main();
-
-    /* Trap if main returns */
-    while (1);
-}
+/* Handlers */
+void Reset_Handler(void);
+void Default_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
 
 /* Vector table */
 __attribute__((section(".isr_vector")))
-void (*vector_table[])(void) = {
-    (void (*)(void))0x20001000, // Initial SP
-    Reset_Handler,             // Reset
-    Default_Handler,           // NMI
-    Default_Handler,           // HardFault
-    Default_Handler,           // MemManage
-    Default_Handler,           // BusFault
-    Default_Handler,           // UsageFault
-    0, 0, 0, 0,                // Reserved
-    Default_Handler,           // SVCall
-    Default_Handler,           // Debug Monitor
-    0,                         // Reserved
-    PendSV_Handler,           // PendSV
-    SysTick_Handler            // SysTick
+void (*const vector_table[])(void) = {
+    (void (*)(void))(&_estack), // 0  Initial stack
+    Reset_Handler,              // 1  Reset
+    Default_Handler,            // 2  NMI
+    Default_Handler,            // 3  HardFault
+    Default_Handler,            // 4  MemManage
+    Default_Handler,            // 5  BusFault
+    Default_Handler,            // 6  UsageFault
+    0, 0, 0, 0,                 // 7–10 Reserved
+    Default_Handler,            // 11 SVCall
+    Default_Handler,            // 12 DebugMonitor
+    0,                          // 13 Reserved
+    PendSV_Handler,             // 14 PendSV  ✅
+    SysTick_Handler             // 15 SysTick ✅
 };
+
+void Reset_Handler(void)
+{
+    main();
+    while (1) {}
+}
+
+void Default_Handler(void)
+{
+    while (1) {}
+}
